ef4634ad698549bd1d3835ac944461e0271f3f7b
Tdlreis <tdlreis@gmail.com>
2023-07-10 22:32:02 -0300

    Não é bug é feature

--- site/validation/views.py
+++ site/validation/views.py
@@ -14,11 +14,13 @@ def validation(request):
         nonValidated = PunchCard.objects.filter(punch_out_time__isnull=False, reviw=False).order_by('punch_in_time')
         for n in nonValidated:
             values = {
+                'name': n.user.user_name,
                 'in': n.punch_in_time,
                 'out': n.punch_out_time,
                 'hours': format_hour((n.punch_out_time - n.punch_in_time).total_seconds()),
                 'id': n.pk,
             }
+            print(values)
             data.append(values)
         context = {
             'data': data,
